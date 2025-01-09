f5342d0aa04ff03752e10b0ff01c35034ab8af79
CubeTheMaster <marco.derosa115@gmail.com>
2025-01-09 12:11:20 +0100

    fixed an error with imports

--- Battle_AI/main.py
+++ Battle_AI/main.py
@@ -1,7 +1,7 @@
 import mysql.connector
-from Pokemon import get_pokemon, select_best_pokemon
-from Mosse import get_moves
-from Battaglia import battle
+from Pokemon import *
+from Mosse import *
+from Battaglia import *
 
 # cambiare la connection in modo da renderla automatizzata
 # database connection
@@ -12,6 +12,59 @@ def connect_to_database():
         password="Araki_1986",
         database="lotta"
     )
+
+def battle(pokemon1, moves1, pokemon2, moves2):
+    hp1 = pokemon1[4]
+    hp2 = pokemon2[4]
+
+    print(f"\nInizia la battaglia: {pokemon1[1]} VS {pokemon2[1]}\n")
+
+    while hp1 > 0 and hp2 > 0:
+        # Determina chi attacca per primo
+        if pokemon1[9] >= pokemon2[9]:
+            attacker, defender = pokemon1, pokemon2
+            attacker_moves, defender_moves = moves1, moves2
+            attacker_hp, defender_hp = hp1, hp2
+        else:
+            attacker, defender = pokemon2, pokemon1
+            attacker_moves, defender_moves = moves2, moves1
+            attacker_hp, defender_hp = hp2, hp1
+
+        # Scegli la mossa dell'attaccante
+        move = select_best_move(attacker_moves, defender)
+
+        print(f"{attacker[1]} usa {move[1]}!")
+
+
+
+        # Calcola l'efficacia di tipo
+        type_effectiveness = calculate_type_advantage(move[2], defender[2], defender[3])
+
+        # Determina se l'attacco è fisico o speciale
+        if move[5] == 'fisica':
+            attack_stat = attacker[5]
+            defense_stat = defender[6]
+        else:
+            attack_stat = attacker[7]
+            defense_stat = defender[8]
+
+        # Calcola il danno
+        damage = ((2 * 50 / 5 + 2) * move[3] * (attack_stat / defense_stat) / 50 + 2) * type_effectiveness
+        defender_hp -= damage
+
+        print(f"{defender[1]} subisce {damage:.2f} danni! Rimangono {max(defender_hp, 0):.2f} HP.")
+
+        # Aggiorna gli HP
+        if attacker == pokemon1:
+            hp2 = defender_hp
+        else:
+            hp1 = defender_hp
+
+        # Controlla se il difensore è stato sconfitto
+        if defender_hp <= 0:
+            print(f"{defender[1]} è stato sconfitto! {attacker[1]} vince la battaglia!\n")
+            break
+
 
 # Main function
 def main():
@@ -51,56 +104,5 @@ if __name__ == "__main__":
 
 # Funcction for the battle
 
-def battle(pokemon1, moves1, pokemon2, moves2):
-    hp1 = pokemon1[4]
-    hp2 = pokemon2[4]
-
-    print(f"\nInizia la battaglia: {pokemon1[1]} VS {pokemon2[1]}\n")
-
-    while hp1 > 0 and hp2 > 0:
-        # Determina chi attacca per primo
-        if pokemon1[9] >= pokemon2[9]:
-            attacker, defender = pokemon1, pokemon2
-            attacker_moves, defender_moves = moves1, moves2
-            attacker_hp, defender_hp = hp1, hp2
-        else:
-            attacker, defender = pokemon2, pokemon1
-            attacker_moves, defender_moves = moves2, moves1
-            attacker_hp, defender_hp = hp2, hp1
-
-        # Scegli la mossa dell'attaccante
-        move = select_best_move(attacker_moves, defender_hp)
-
-        print(f"{attacker[1]} usa {move[1]}!")
-
-
-
-        # Calcola l'efficacia di tipo
-        type_effectiveness = calculate_type_advantage(move[2], defender[2], defender[3])
-
-        # Determina se l'attacco è fisico o speciale
-        if move[5] == 'fisica':
-            attack_stat = attacker[5]
-            defense_stat = defender[6]
-        else:
-            attack_stat = attacker[7]
-            defense_stat = defender[8]
-
-        # Calcola il danno
-        damage = ((2 * 50 / 5 + 2) * move[3] * (attack_stat / defense_stat) / 50 + 2) * type_effectiveness
-        defender_hp -= damage
-
-        print(f"{defender[1]} subisce {damage:.2f} danni! Rimangono {max(defender_hp, 0):.2f} HP.")
-
-        # Aggiorna gli HP
-        if attacker == pokemon1:
-            hp2 = defender_hp
-        else:
-            hp1 = defender_hp
-
-        # Controlla se il difensore è stato sconfitto
-        if defender_hp <= 0:
-            print(f"{defender[1]} è stato sconfitto! {attacker[1]} vince la battaglia!\n")
-            break
 
 
